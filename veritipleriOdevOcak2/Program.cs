b0b4f31bf636508734049d0966188ffc6ccc8bc7
halilemre <halilemretopaloglu@gmail.com>
2025-03-16 05:59:06 +0300

    Proje dosyası ekle.

---
+++ veritipleriOdevOcak2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        double sayi1, sayi2, toplam;
+
+        
+        Console.Write("Birinci sayıyı giriniz: ");
+
+        
+        while (!double.TryParse(Console.ReadLine(), out sayi1))
+        {
+            Console.Write("Geçersiz giriş! Lütfen sayısal bir değer giriniz: ");
+        }
+
+       
+        Console.Write("İkinci sayıyı giriniz: ");
+
+       
+        while (!double.TryParse(Console.ReadLine(), out sayi2))
+        {
+            Console.Write("Geçersiz giriş! Lütfen sayısal bir değer giriniz: ");
+        }
+
+        
+        toplam = sayi1 + sayi2;
+
+      
+        Console.WriteLine($"Girdiğiniz sayıların toplamı: {toplam}");
+
+        Console.WriteLine("{0} + {1} = {2}", sayi1, sayi2, toplam);
+    }
+}
